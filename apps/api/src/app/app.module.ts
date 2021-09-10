b620e8ae99c5719c48ac45ca3814ebbd0d7e6968
Nihad KV <nihad083@gmail.com>
2021-09-11 01:26:23 +0530

    todo api implemented

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/app/todo/todo.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TodoEntity } from './models/todo.entity';
+import { TodoModel } from './models/todo.model';
+
+@Injectable()
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoEntity: Repository<TodoEntity>
+  ) {}
+  createTask(task: TodoModel) {
+    return this.todoEntity.save(task);
+  }
+
+  getTask() {
+    return this.todoEntity.find();
+  }
+
+  updateTask(id: number, task: TodoModel) {
+    return this.todoEntity.update(id, task);
+  }
+
+  deleteTask(id: number) {
+    return this.todoEntity.delete(id);
+  }
+}
